6a7e99dab83a79cff3f8752308d9ad2dd23a5e2a
Jimmy Nguyen <dtstego@gmail.com>
2022-10-25 21:23:03 -0700

    Added static variables for width and height of screen which are initialized at boot time. Used to set the size of the program.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    // Size of screen, initialized on program startup and is scaled using screenScale
+    public static double screenWidth;
+    public static double screenHeight;
+    // Default scale for size of stage; Is multiplied with screenWidth and screenHeight
+    public static double screenScale = 0.75;
+
     @Override
     public void start(Stage primaryStage)
     {
         Group root = new Group();
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, screenWidth * screenScale, screenHeight * screenScale);
 
-        primaryStage.setTitle("My JavaFX Application");
+        primaryStage.setTitle("Flashcard Manager");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args)
     {
+        // At startup, sets the values of screenWidth and screenHeight to monitor size
+        Rectangle2D bounds = Screen.getPrimary().getBounds();
+        screenWidth = bounds.getWidth();
+        screenHeight = bounds.getHeight();
+
         launch(args);
     }
 }
