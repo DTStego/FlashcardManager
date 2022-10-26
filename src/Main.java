57ab85d1825d317999a1fd90f466255f71ea1af5
Jimmy Nguyen <dtstego@gmail.com>
2022-10-25 19:45:59 -0700

    Added a sample JavaFX project that should work with any Java 8 JDK that includes JavaFX

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("My JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
