6d283c40d80fe2cc73e275b6fecbe173853049c0
Jimmy <dtstego@gmail.com>
2022-10-27 03:36:25 -0700

    Developed the User class with TODO for getters, setters, hashcode, and equals methods.
    Created a basic UserDatabase class and looking for implementations to store User objects.

---
+++ src/application/User.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.Serializable;
+
+public class User implements Serializable
+{
+    // The password and securityAnswer variables should generally be passed into an encryptor
+    // before being set to the instance variables. The encryptor will be provided in a later release.
+
+    private String username;
+    private String password;
+    private String securityQuestion;
+    private String securityAnswer;
+
+    public User(String username, String password, String securityQuestion, String securityAnswer)
+    {
+        this.username = username;
+        this.password = password;
+        this.securityQuestion = securityQuestion;
+        this.securityAnswer = securityQuestion;
+    }
+
+    /* TODO
+    Implement Getter/Setter Methods
+    Implement Hashcode/Equals method so Users are stored in a hashmap database
+     */
+}
---
+++ src/application/UserDatabase.java
@@ -0,0 +1,10 @@
+package application;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+// UserDatabase is initialized at the start of the program from an external file/system
+public class UserDatabase implements Serializable
+{
+
+}
