2e63c569e70d89cc2af3b5f31bf8cd7744f3b53e
Jimmy Nguyen <dtstego@gmail.com>
2022-10-27 06:17:40 -0700

    Implemented getters/setters for User class

--- src/application/User.java
+++ src/application/User.java
@@ -13,6 +13,16 @@ public class User implements Serializable
     private String securityQuestion;
     private String securityAnswer;
 
+    /*
+        Issue: If a User class in userList changes its username or other variable that hashCode()
+               or equals() was dependent on, it would mess up the HashSet, i.e., using mutable/changeable
+               variables for the equals() method is "iffy".
+
+        Solution: Stop mutating the objects while they are in a hash set.
+                  Remove them before you mutate them, put them back in later.
+        If we change a user's username, password, etc., remove them from the userList first and add them after!
+     */
+
     public User(String username, String password, String securityQuestion, String securityAnswer)
     {
         this.username = username;
@@ -22,7 +32,46 @@ public class User implements Serializable
     }
 
     /* TODO
-    Implement Getter/Setter Methods
     Implement Hashcode/Equals method so Users are stored in a hashmap database
-     */
+    */
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getSecurityQuestion()
+    {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion)
+    {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getSecurityAnswer()
+    {
+        return securityAnswer;
+    }
+
+    public void setSecurityAnswer(String securityAnswer)
+    {
+        this.securityAnswer = securityAnswer;
+    }
 }
--- src/application/UserDatabase.java
+++ src/application/UserDatabase.java
@@ -6,15 +6,6 @@ import java.util.HashSet;
 // UserDatabase is initialized at the start of the program from an external file/system
 public class UserDatabase implements Serializable
 {
-    /*
-        Issue: If a User class in userList changes its username or other variable that hashCode()
-               or equals() was dependent on, it would mess up the HashSet, i.e., using mutable/changeable
-               variables for the equals() method is "iffy".
-
-        Solution: Stop mutating the objects while they are in a hash set.
-                  Remove them before you mutate them, put them back in later.
-        If we change a user's username, password, etc., remove them from the userList first and add them after!
-     */
     private HashSet<User> userList;
 
     public UserDatabase()
@@ -22,18 +13,13 @@ public class UserDatabase implements Serializable
 
     }
 
-    public void addUser()
+    public void addUser(User user)
     {
-
+        userList.add(user);
     }
 
     public void deleteUser(User user)
     {
-
-    }
-
-    public void changePassword(User user, String newPassword)
-    {
-
+        userList.remove(user);
     }
 }
