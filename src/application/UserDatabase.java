767aff0aaf4448778eff7f0ddddfe28dfdb8cbd8
Jimmy Nguyen <dtstego@gmail.com>
2022-10-27 06:28:56 -0700

    Added getter for userList.
    Added UserDatabase constructor with a hashset parameter; The database should be built at startup through a future UserPersistence implementation which retrieves a hashset (userList) from a file/sql database or returns an empty userList.

--- src/application/UserDatabase.java
+++ src/application/UserDatabase.java
@@ -8,18 +8,32 @@ public class UserDatabase implements Serializable
 {
     private HashSet<User> userList;
 
-    public UserDatabase()
+    // UserDataBase should be built at startup through a UserPersistence interface implementation
+    public UserDatabase(HashSet<User> userList)
     {
-
+        this.userList = userList;
     }
 
     public void addUser(User user)
     {
         userList.add(user);
+
+        /* TODO
+           Add code to serialize or save the database after a new user is added to the database
+         */
     }
 
     public void deleteUser(User user)
     {
         userList.remove(user);
+
+        /* TODO
+           Add code to serialize or save the database after a user is deleted from the database
+         */
+    }
+
+    public HashSet<User> getUserList()
+    {
+        return userList;
     }
 }
