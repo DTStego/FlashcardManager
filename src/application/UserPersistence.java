bb557b11c726de5e41a63ba2340541c43809ccf5
Jimmy Nguyen <dtstego@gmail.com>
2022-10-27 22:48:34 -0700

    Implemented a createSaveFile method for UserSerialize which attempts to create a save file in "Documents/FlashcardManager/saves/" called userList.txt.

--- src/application/UserPersistence.java
+++ src/application/UserPersistence.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.io.IOException;
 import java.util.HashSet;
 
 public interface UserPersistence
 {
     void save(HashSet<User> user);
     HashSet<User> retrieve();
+    boolean createSaveFile() throws IOException;
 }
--- src/application/UserSerialize.java
+++ src/application/UserSerialize.java
@@ -1,11 +1,19 @@
 package application;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 
 public class UserSerialize implements UserPersistence
 {
+    /**
+     * Stores the userList in a "saves" folder through serialization
+     * Creates a "saves" folder and "UserList.txt" file if none are found
+     * @param userList HashSet<User> userList to be stored by serializing
+     */
     @Override
-    public void save(HashSet<User> user)
+    public void save(HashSet<User> userList)
     {
 
     }
@@ -14,5 +22,30 @@ public class UserSerialize implements UserPersistence
     public HashSet<User> retrieve()
     {
         return null;
+    }
+
+    /**
+     * Creates a Folder/File structure in the Application package if none is found
+     * Saves to ../My Documents/FlashcardManager/saves/userList.txt
+     * @return true if file was created; false if file already exists
+     */
+    @Override
+    public boolean createSaveFile() throws IOException
+    {
+        // Path of save folder in documents, e.g., (For Windows) "../My Documents/FlashcardManager/saves/"
+        String saveLocation = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
+                .concat(File.separatorChar + "FlashcardManager" + File.separatorChar + "saves");
+
+        File file = new File(saveLocation);
+
+        // Attempt to create the directory "FlashcardManager/saves"
+        // Cannot combine with .createNewFile statement because method creates files instead
+        file.mkdirs();
+
+        // Adjust file pointer to the actual .txt file (Which stores the userList database)
+        file = new File(saveLocation.concat(File.separatorChar + "userList.txt"));
+
+        // Return if successful
+        return file.createNewFile();
     }
 }
