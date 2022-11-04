86cecd7a53eb1f5dd4f3d482a97aecd2568d5a8a
Keigo Tajima <keigo.tajima@sjsu.edu>
2022-11-03 21:08:19 -0700

    Added back button. Code is for returnToLogin() empty until login page is completed

--- src/application/register/RegisterController.java
+++ src/application/register/RegisterController.java
@@ -3,6 +3,7 @@ package application.register;
 import application.Main;
 import application.User;
 import application.managers.Notebook;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -13,14 +14,20 @@ public class RegisterController {
     public TextField security;
     public TextField secAnswer;
     public Label errorLBL;
+    public Button backBtn;
 
+
+    /** Return to login page*/
+    public void returnToLogin() {
+        //return to loginpage
+        System.out.println("return to login");
+    }
     /** Returns true if username is unique and not in the database */
     public boolean isValid() {
         //Reset errorMsg
         String errorMsg = "";
         // Invalid variable is incremented if any fields are empty or if the username is not unique
         int invalid = 0;
-
         //Check if username is not blank and is unique
         if (usernameInput.getLength()==0 || Main.userDatabase.getUserList().containsKey(usernameInput.getText())) {
             errorMsg += "* Invalid username, select a different username\n";
@@ -51,7 +58,6 @@ public class RegisterController {
             errorLBL.setText("");   //this is purely optional and can be removed
             return true;
         }
-
         /* Old code without that checks if valid without sending error message if invalid */
 //        if(usernameInput.getLength()!=0 && passwordInput.getLength()!=0 && security.getLength()!=0 && secAnswer.getLength()!=0) {
 //            //Return true if username is not in userDatabase
