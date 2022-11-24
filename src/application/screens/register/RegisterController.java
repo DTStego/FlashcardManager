8f8b360ff0e82501684f59281df1a42ef72dba4d
Jimmy Nguyen <dtstego@gmail.com>
2022-11-24 04:15:40 -0800

    Adjusted code style for RegisterController and fixed formatting issues.

--- src/application/screens/register/RegisterController.java
+++ src/application/screens/register/RegisterController.java
@@ -23,7 +23,7 @@ public class RegisterController
     @FXML
     private Label errorLBL;
 
-    /** Return to login page*/
+    /** Return to the login page */
     @FXML
     void returnToLogin(ActionEvent event)
     {
@@ -47,34 +47,40 @@ public class RegisterController
     {
         // Reset errorMsg
         String errorMsg = "";
+
         // Invalid variable is incremented if any fields are empty or if the username is not unique
         int invalid = 0;
+
         // Check if username is not blank and is unique
         if (usernameInput.getLength() == 0 || Main.userDatabase.contains(usernameInput.getText()))
         {
             errorMsg += "* Invalid username, select a different username\n";
             invalid++;
         }
-        // Check if password is not blank
+
+        // Check if password field is not blank
         if (passwordInput.getLength() == 0)
         {
             errorMsg += "* Password must be of length greater than 0\n";
             invalid++;
         }
+
         // Check if security question is not blank
         if (security.getLength() == 0)
         {
             errorMsg += "* Please set a security question\n";
             invalid++;
         }
+
         // Check if security answer is not blank
         if (secAnswer.getLength() == 0)
         {
             errorMsg += "* Please input the answer to the security question\n";
             invalid++;
         }
+
         // If invalid variable is not 0, then display error message and return false
-        if(invalid != 0)
+        if (invalid != 0)
         {
             errorLBL.setText(errorMsg);
             return false;
@@ -82,13 +88,7 @@ public class RegisterController
         // User is valid, return true
         else
         {
-            errorLBL.setText("");   //this is purely optional and can be removed
             return true;
         }
-        /* Old code without that checks if valid without sending error message if invalid */
-//        if(usernameInput.getLength()!=0 && passwordInput.getLength()!=0 && security.getLength()!=0 && secAnswer.getLength()!=0) {
-//            //Return true if username is not in userDatabase
-//            return !Main.userDatabase.getUserList().containsKey(usernameInput.getText());
-//        }
     }
 }
